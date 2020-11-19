6e3dc81ac84a154efbc8a035ee9ee21cc537208d
Anders <joensson.anders@gmail.com>
2020-11-19 23:54:26 +0100

    Updating dependencies to the latest

--- dist/index.js
+++ dist/index.js
@@ -1,187 +1,11 @@
 module.exports =
-/******/ (function(modules, runtime) { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	__webpack_require__.ab = __dirname + "/";
-/******/
-/******/ 	// the startup function
-/******/ 	function startup() {
-/******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(104);
-/******/ 	};
-/******/
-/******/ 	// run startup
-/******/ 	return startup();
-/******/ })
-/************************************************************************/
-/******/ ({
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-/***/ 43:
-/***/ (function(module, __unusedexports, __webpack_require__) {
+/***/ 932:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-const https = __webpack_require__(211);
-
-const getOptions = (token) => {
-  return {
-    hostname: "slack.com",
-    port: 443,
-    path: "/api/chat.postMessage",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      Authorization: `Bearer ${token}`,
-    },
-  };
-};
-
-const post = (token, message) => {
-  return new Promise((resolve, reject) => {
-    const payload = JSON.stringify(message);
-
-    console.log("DEBUG: Payload", payload);
-
-    const options = getOptions(token);
-
-    const req = https.request(options, (res) => {
-      const chunks = [];
-
-      res.on("data", (chunk) => {
-        chunks.push(chunk);
-      });
-
-      res.on("end", () => {
-        resolve({
-          statusCode: res.statusCode,
-          result: Buffer.concat(chunks).toString(),
-        });
-      });
-    });
-
-    req.on("error", (error) => {
-      reject(error);
-    });
-
-    req.write(payload);
-    req.end();
-  });
-};
-
-const sendMessage = async (token, message) => {
-  const response = await post(token, message);
-  const result = JSON.parse(response.result);
-
-  if (!result || !result.ok || response.statusCode !== 200) {
-    throw `Error! ${JSON.stringify(response)}`;
-  }
-
-  return response;
-};
-
-module.exports = sendMessage;
-
-
-/***/ }),
-
-/***/ 82:
-/***/ (function(__unusedmodule, exports) {
-
-"use strict";
-
-// We use any as a valid input type
-/* eslint-disable @typescript-eslint/no-explicit-any */
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Sanitizes an input into a string so it can be passed into issueCommand safely
- * @param input input to sanitize into a string
- */
-function toCommandValue(input) {
-    if (input === null || input === undefined) {
-        return '';
-    }
-    else if (typeof input === 'string' || input instanceof String) {
-        return input;
-    }
-    return JSON.stringify(input);
-}
-exports.toCommandValue = toCommandValue;
-//# sourceMappingURL=utils.js.map
-
-/***/ }),
-
-/***/ 87:
-/***/ (function(module) {
-
-module.exports = require("os");
-
-/***/ }),
-
-/***/ 102:
-/***/ (function(__unusedmodule, exports, __webpack_require__) {
-
-"use strict";
-
-// For internal use, subject to change.
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// We use any as a valid input type
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const fs = __importStar(__webpack_require__(747));
-const os = __importStar(__webpack_require__(87));
-const utils_1 = __webpack_require__(82);
-function issueCommand(command, message) {
-    const filePath = process.env[`GITHUB_${command}`];
-    if (!filePath) {
-        throw new Error(`Unable to find environment variable for file command ${command}`);
-    }
-    if (!fs.existsSync(filePath)) {
-        throw new Error(`Missing file at path: ${filePath}`);
-    }
-    fs.appendFileSync(filePath, `${utils_1.toCommandValue(message)}${os.EOL}`, {
-        encoding: 'utf8'
-    });
-}
-exports.issueCommand = issueCommand;
-//# sourceMappingURL=file-command.js.map
-
-/***/ }),
-
-/***/ 104:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-const invoke = __webpack_require__(137);
+const invoke = __webpack_require__(662);
 
 const run = async () => {
   await invoke();
@@ -192,90 +16,8 @@ run();
 
 /***/ }),
 
-/***/ 137:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-const context = __webpack_require__(450);
-const postMessage = __webpack_require__(43);
-const buildMessage = __webpack_require__(325);
-
-const invoke = async () => {
-  try {
-    const token = context.getRequired("slack-bot-user-oauth-access-token");
-    const channel = context.getRequired("slack-channel");
-    const text = context.getRequired("slack-text");
-
-    const message = buildMessage(channel, text, optional());
-    const result = await postMessage(token, message);
-
-    const resultAsJson = stringify(result);
-
-    context.setOutput("slack-result", resultAsJson);
-  } catch (error) {
-    context.setFailed(stringify(error));
-  }
-};
-
-const optional = () => {
-  let opt = {};
-
-  const env = context.getEnv();
-  Object.keys(env)
-    .filter((key) => !!env[key])
-    .filter((key) => key.toUpperCase().startsWith("INPUT_SLACK-OPTIONAL-"))
-    .forEach((key) => {
-      const slackKey = key.replace("INPUT_SLACK-OPTIONAL-", "").toLowerCase();
-      opt[slackKey] = env[key];
-    });
-
-  return opt;
-};
-
-const stringify = (data) => JSON.stringify(data, undefined, 2);
-
-module.exports = invoke;
-
-
-/***/ }),
-
-/***/ 211:
-/***/ (function(module) {
-
-module.exports = require("https");
-
-/***/ }),
-
-/***/ 325:
-/***/ (function(module) {
-
-const buildMessage = (channel = "", text = "", optional = {}) => {
-  const message = {
-    channel,
-    text,
-  };
-
-  message.text = restoreEscapedNewLine(message.text);
-  message.text = restoreEscapedTab(message.text);
-
-  Object.keys(optional).forEach((name) => {
-    message[name] = optional[name];
-  });
-
-  return message;
-};
-
-const restoreEscapedNewLine = (text) =>
-  text.replace(/\\r\\n/g, "\n").replace(/\\n/g, "\n");
-
-const restoreEscapedTab = (text) => text.replace(/\\t/g, "\t");
-
-module.exports = buildMessage;
-
-
-/***/ }),
-
-/***/ 431:
-/***/ (function(__unusedmodule, exports, __webpack_require__) {
+/***/ 351:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 "use strict";
 
@@ -286,9 +28,9 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 const os = __importStar(__webpack_require__(87));
-const utils_1 = __webpack_require__(82);
+const utils_1 = __webpack_require__(278);
 /**
  * Commands
  *
@@ -360,31 +102,8 @@ function escapeProperty(s) {
 
 /***/ }),
 
-/***/ 450:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-const core = __webpack_require__(470);
-
-const getRequired = name => core.getInput(name, { required: true });
-
-const getEnv = () => process.env;
-
-const setOutput = (...args) => core.setOutput(args);
-
-const setFailed = (...args) => core.setFailed(args);
-
-module.exports = {
-  getRequired,
-  getEnv,
-  setOutput,
-  setFailed
-};
-
-
-/***/ }),
-
-/***/ 470:
-/***/ (function(__unusedmodule, exports, __webpack_require__) {
+/***/ 186:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 "use strict";
 
@@ -404,10 +123,10 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const command_1 = __webpack_require__(431);
-const file_command_1 = __webpack_require__(102);
-const utils_1 = __webpack_require__(82);
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const command_1 = __webpack_require__(351);
+const file_command_1 = __webpack_require__(717);
+const utils_1 = __webpack_require__(278);
 const os = __importStar(__webpack_require__(87));
 const path = __importStar(__webpack_require__(622));
 /**
@@ -628,18 +347,302 @@ exports.getState = getState;
 
 /***/ }),
 
-/***/ 622:
-/***/ (function(module) {
+/***/ 717:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-module.exports = require("path");
+"use strict";
+
+// For internal use, subject to change.
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const fs = __importStar(__webpack_require__(747));
+const os = __importStar(__webpack_require__(87));
+const utils_1 = __webpack_require__(278);
+function issueCommand(command, message) {
+    const filePath = process.env[`GITHUB_${command}`];
+    if (!filePath) {
+        throw new Error(`Unable to find environment variable for file command ${command}`);
+    }
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`Missing file at path: ${filePath}`);
+    }
+    fs.appendFileSync(filePath, `${utils_1.toCommandValue(message)}${os.EOL}`, {
+        encoding: 'utf8'
+    });
+}
+exports.issueCommand = issueCommand;
+//# sourceMappingURL=file-command.js.map
+
+/***/ }),
+
+/***/ 278:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+/**
+ * Sanitizes an input into a string so it can be passed into issueCommand safely
+ * @param input input to sanitize into a string
+ */
+function toCommandValue(input) {
+    if (input === null || input === undefined) {
+        return '';
+    }
+    else if (typeof input === 'string' || input instanceof String) {
+        return input;
+    }
+    return JSON.stringify(input);
+}
+exports.toCommandValue = toCommandValue;
+//# sourceMappingURL=utils.js.map
+
+/***/ }),
+
+/***/ 284:
+/***/ ((module) => {
+
+const buildMessage = (channel = "", text = "", optional = {}) => {
+  const message = {
+    channel,
+    text,
+  };
+
+  message.text = restoreEscapedNewLine(message.text);
+  message.text = restoreEscapedTab(message.text);
+
+  Object.keys(optional).forEach((name) => {
+    message[name] = optional[name];
+  });
+
+  return message;
+};
+
+const restoreEscapedNewLine = (text) =>
+  text.replace(/\\r\\n/g, "\n").replace(/\\n/g, "\n");
+
+const restoreEscapedTab = (text) => text.replace(/\\t/g, "\t");
+
+module.exports = buildMessage;
+
+
+/***/ }),
+
+/***/ 319:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const core = __webpack_require__(186);
+
+const getRequired = name => core.getInput(name, { required: true });
+
+const getEnv = () => process.env;
+
+const setOutput = (...args) => core.setOutput(args);
+
+const setFailed = (...args) => core.setFailed(args);
+
+module.exports = {
+  getRequired,
+  getEnv,
+  setOutput,
+  setFailed
+};
+
+
+/***/ }),
+
+/***/ 662:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const context = __webpack_require__(319);
+const postMessage = __webpack_require__(132);
+const buildMessage = __webpack_require__(284);
+
+const invoke = async () => {
+  try {
+    const token = context.getRequired("slack-bot-user-oauth-access-token");
+    const channel = context.getRequired("slack-channel");
+    const text = context.getRequired("slack-text");
+
+    const message = buildMessage(channel, text, optional());
+    const result = await postMessage(token, message);
+
+    const resultAsJson = stringify(result);
+
+    context.setOutput("slack-result", resultAsJson);
+  } catch (error) {
+    context.setFailed(stringify(error));
+  }
+};
+
+const optional = () => {
+  let opt = {};
+
+  const env = context.getEnv();
+  Object.keys(env)
+    .filter((key) => !!env[key])
+    .filter((key) => key.toUpperCase().startsWith("INPUT_SLACK-OPTIONAL-"))
+    .forEach((key) => {
+      const slackKey = key.replace("INPUT_SLACK-OPTIONAL-", "").toLowerCase();
+      opt[slackKey] = env[key];
+    });
+
+  return opt;
+};
+
+const stringify = (data) => JSON.stringify(data, undefined, 2);
+
+module.exports = invoke;
+
+
+/***/ }),
+
+/***/ 132:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const https = __webpack_require__(211);
+
+const getOptions = (token) => {
+  return {
+    hostname: "slack.com",
+    port: 443,
+    path: "/api/chat.postMessage",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
+const post = (token, message) => {
+  return new Promise((resolve, reject) => {
+    const payload = JSON.stringify(message);
+
+    console.log("DEBUG: Payload", payload);
+
+    const options = getOptions(token);
+
+    const req = https.request(options, (res) => {
+      const chunks = [];
+
+      res.on("data", (chunk) => {
+        chunks.push(chunk);
+      });
+
+      res.on("end", () => {
+        resolve({
+          statusCode: res.statusCode,
+          result: Buffer.concat(chunks).toString(),
+        });
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+    });
+
+    req.write(payload);
+    req.end();
+  });
+};
+
+const sendMessage = async (token, message) => {
+  const response = await post(token, message);
+  const result = JSON.parse(response.result);
+
+  if (!result || !result.ok || response.statusCode !== 200) {
+    throw `Error! ${JSON.stringify(response)}`;
+  }
+
+  return response;
+};
+
+module.exports = sendMessage;
+
 
 /***/ }),
 
 /***/ 747:
-/***/ (function(module) {
+/***/ ((module) => {
 
-module.exports = require("fs");
+"use strict";
+module.exports = require("fs");;
+
+/***/ }),
+
+/***/ 211:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("https");;
+
+/***/ }),
+
+/***/ 87:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("os");;
+
+/***/ }),
+
+/***/ 622:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("path");;
 
 /***/ })
 
-/******/ });
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	__webpack_require__.ab = __dirname + "/";/************************************************************************/
+/******/ 	// module exports must be returned from runtime so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(932);
+/******/ })()
+;
