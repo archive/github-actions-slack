fc907c85b9543ff18d09949fef343655589acc65
Anders <joensson.anders@gmail.com>
2022-10-14 23:39:00 +0200

    build

--- src/integration/slack-api-post.js
+++ src/integration/slack-api-post.js
@@ -17,6 +17,9 @@ const post = (token, path, message) => {
   return new Promise((resolve, reject) => {
     const payload = JSON.stringify(message);
 
+    console.log("PAYLOAD101");
+    console.log(message);
+
     const options = getOptions(token, path);
 
     const req = https.request(options, (res) => {
