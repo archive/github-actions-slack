a2090c1e609eca2f5ed150c3347fed00d9200820
Anders <joensson.anders@gmail.com>
2022-10-14 23:49:36 +0200

    added missing ts to test and moved ts above text/blocks

--- src/update-message/build-update-message.js
+++ src/update-message/build-update-message.js
@@ -3,7 +3,21 @@ const {
   restoreEscapedTab,
 } = require("../util/escaper.js");
 
-const buildMessage = (channel = "", text = "", blocks = "", ts = "", optional = {}) => {
+const buildMessage = (
+  channel = "",
+  text = "",
+  blocks = "",
+  ts = "",
+  optional = {}
+) => {
+  if (!channel || !ts) {
+    throw new Error("Channel and/or TS must be set");
+  }
+
+  if (!text && !blocks) {
+    throw new Error("Text OR Block must be set");
+  }
+
   const message = {
     channel,
     text,
